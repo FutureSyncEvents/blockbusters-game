eef1036c486c718facb58494991cc1f97121b535
ionescuea <60351947+ionescuea@users.noreply.github.com>
2020-01-29 10:38:19 +0000

    Convert Ascii to character in JS

--- Blockbusters.js
+++ Blockbusters.js
@@ -1,42 +1,45 @@
 var questions = {
-  '65': 'Question for letter A',
-  '66': 'Question for letter B',
-  '67': 'Question for letter C',
-  '68': 'Question for letter D',
-  '69': 'Question for letter E',
-  '70': 'Question for letter F',
-  '71': 'Question for letter G',
-  '72': 'Question for letter H',
-  '73': 'Question for letter I',
-  '74': 'Question for letter J',
-  '75': 'Question for letter K',
-  '76': 'Question for letter L',
-  '77': 'Question for letter M',
-  '78': 'Question for letter N',
-  '79': 'Question for letter O',
-  '80': 'Question for letter P',
-  '81': 'Question for letter Q',
-  '82': 'Question for letter R',
-  '83': 'Question for letter S',
-  '84': 'Question for letter T',
-  '85': 'Question for letter U',
-  '86': 'Question for letter V',
-  '87': 'Question for letter W',
-  '88': 'Question for letter X',
-  '89': 'Question for letter Y',
-  '90': 'Question for letter Z',
+  'A': 'Question for letter A',
+  'B': 'Question for letter B',
+  'C': 'Question for letter C',
+  'D': 'Question for letter D',
+  'E': 'Question for letter E',
+  'F': 'Question for letter F',
+  'G': 'Question for letter G',
+  'H': 'Question for letter H',
+  'I': 'Question for letter I',
+  'J': 'Question for letter J',
+  'K': 'Question for letter K',
+  'L': 'Question for letter L',
+  'M': 'Question for letter M',
+  'N': 'Question for letter N',
+  'O': 'Question for letter O',
+  'P': 'Question for letter P',
+  'Q': 'Question for letter Q',
+  'R': 'Question for letter R',
+  'S': 'Question for letter S',
+  'T': 'Question for letter T',
+  'U': 'Question for letter U',
+  'V': 'Question for letter V',
+  'W': 'Question for letter W',
+  'X': 'Question for letter X',
+  'Y': 'Question for letter Y',
+  'Z': 'Question for letter Z',
 };
 
 // update by key press
 document.addEventListener("keydown", event => {
-  var keypress = event.keyCode
   if (event.isComposing || event.keyCode >= 65 && event.keyCode <= 90) {
-    document.getElementById('question').innerHTML = questions[keypress];
+    var code = event.keyCode
+    var letter = String.fromCharCode(code)
+    document.getElementById('question').innerHTML = questions[letter];
   }
 });
 
 // update by click
 document.addEventListener("click", event => {
-  var clickpress = event.target.text.charCodeAt(0)
-  document.getElementById('question').innerHTML = questions[clickpress];
+  var letter = event.target.text
+  if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+    document.getElementById('question').innerHTML = questions[letter];
+  }
 });
